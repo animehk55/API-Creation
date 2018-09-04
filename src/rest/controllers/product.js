3f9841c9fb7c5df4a4775cf8259a1920ee149809
Animesh <animeshk55@gmail.com>
2018-09-05 05:10:53 +0530

    add the product endpoint with mdifications

--- src/rest/controllers/product.js
+++ src/rest/controllers/product.js
@@ -7,3 +7,10 @@ export const productList = async(req, res) => {
     res.json({message: 'Product list displayed successfully', List: list})
 }
 
+export const addProduct = async(req, res) => {
+    const {plantName, scientificName, light, height, discountPercentage, price, uses, quantity, symbol, description} = req.body
+
+    const data = await productQueries.addProduct(db, req.body)
+    
+    res.json({message: 'Product added successfully'})
+}
