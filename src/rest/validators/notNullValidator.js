2ad138eba7f1939f6a3250279609adb5fe09807d
Animesh <animeshk55@gmail.com>
2018-09-05 05:12:22 +0530

    Added basics validator"

--- src/rest/validators/notNullValidator.js
+++ src/rest/validators/notNullValidator.js
@@ -2,6 +2,10 @@ import err from '../helpers/error'
 
 export default (req) => {
     const { plantName } = req.body
-
+    
+	if (!plantName) {
+		return err('Please provide a valid plant name.', 'plantName')
+    }
+    
     return null
 }
